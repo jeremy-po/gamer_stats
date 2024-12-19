a8ecb646545f6ad07680425f3625408f1659315e
jeremy-po <jeremypoliver@hotmail.com>
2024-12-18 20:05:55 -0400

    test

--- js/lol.js
+++ js/lol.js
@@ -1,5 +1,5 @@
 async function fetchSummonerData(summonerName) {
-    const response = await fetch('/gamer_stats/functions/riotProxy', {
+    const response = await fetch('/.netlify/functions/riotProxy', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
